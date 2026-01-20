5eecde71b02f6ad6fb4d1ecc3e968e65899c5758
smparkliv <smpark@livsmed.com>
2026-01-20 22:08:15 +0900

    [FEAT] added to net_injection_power and calc_admittance_matrix funtion

--- function.py
+++ function.py
@@ -4,6 +4,7 @@ import pandas as pd
 def read_data_file(file_path, headers) :
     try : 
         RESULT_MATRIX = pd.read_csv(file_path, sep='\t', header=None)
+        # [TBD] datatype 확인 및 변환 필요시 처리
 
         # row,columns shape 확인
         rows, cols = RESULT_MATRIX.shape
@@ -20,6 +21,41 @@ def read_data_file(file_path, headers) :
         print(f"An error occurred while reading the file : {e}")
         return None    
 
+def get_net_injection_power(P_g, Q_g, P_L, Q_L) :
+    P_net = P_g - P_L
+    Q_net = Q_g - Q_L
+    # print(type(P_net))
+    return P_net, Q_net
+    
+def calc_admittance_matrix(BUS_DATA, LINE_DATA) :
+    # Initialize Y_bus matrix
+    Y_bus = np.zeros((BUS_DATA.shape[0], BUS_DATA.shape[0]), dtype=complex)
+    
+    for i in range(LINE_DATA.shape[0]) :
+        k = int(LINE_DATA.iloc[i]['ID_from']) - 1  # Adjusting for zero-based index
+        n = int(LINE_DATA.iloc[i]['ID_to']) - 1    # Adjusting for zero-based index
+        
+        R = LINE_DATA.iloc[i]['R']
+        X = LINE_DATA.iloc[i]['X']
+        B = LINE_DATA.iloc[i]['B']
+        Z = complex(R, X)
+        Y = 1 / Z
+        B_shunt = complex(0, B / 2)
+        
+        # Self-admittance
+        Y_bus[k, k] += Y + B_shunt
+        Y_bus[n, n] += Y + B_shunt
+        # Mutual admittance
+        Y_bus[k, n] -= Y
+        Y_bus[n, k] -= Y
+        
+        # debugging print
+        # Y_bus_df = pd.DataFrame(Y_bus, index=BUS_DATA['ID'], columns=BUS_DATA['ID'])
+        # print(f"After processing line {i+1} (from bus {k+1} to bus {n+1}):")
+        # print(Y_bus_df)
+
+    return Y_bus
+
 def input_matrix() :
     while True : 
         try : 
@@ -35,15 +71,15 @@ def input_matrix() :
             MATRIX = np.array(number_list_A).reshape(-1, int(MAT_ROW))
             
             #error except process : TBD
-            
             return MATRIX
         
         except ValueError :
             print("[ERROR] : Please enter only numbers and follow the format. try agiain")
-            
+            return None
         except Exception as e : 
             print(f"[ERROR]_{e} : Please enter only numbers and follow the format. try agiain")
-            
+            return None
+        
 def matrix_judgment (mat_a, mat_b) :
     if mat_a.shape[1] == mat_b.shape[0]:
         return True
--- main.py
+++ main.py
@@ -6,15 +6,26 @@ def main() :
     INF_DICT_BUS = {"path": "DATA\Bus.txt", "headers": ["ID", "Bus-type", "P_g", "Q_g", "P_L", "Q_L", "|Vi|", "δi"] }
     INF_DICT_LINE = {"path": "DATA\Line.txt", "headers": ["ID_from", "ID_to", "R", "X", "B"] }
     
-    BUS_VAL = read_data_file(INF_DICT_BUS.get('path'), INF_DICT_BUS.get('headers'))
-    LINE_VAL = read_data_file(INF_DICT_LINE.get('path'), INF_DICT_LINE.get('headers'))
+    # Read data files (DataFrame form)
+    BUS_DATA = read_data_file(INF_DICT_BUS.get('path'), INF_DICT_BUS.get('headers'))
+    LINE_DATA = read_data_file(INF_DICT_LINE.get('path'), INF_DICT_LINE.get('headers'))
     
-    print("<------------Bus data------------>")
-    print(BUS_VAL)
-    print("<------------Line data------------>")
-    print(LINE_VAL)
+    # print("<------------Bus data------------>")
+    # print(BUS_DATA)
+    # print("<------------Line data------------>")
+    # print(LINE_DATA)
+
+    # Calculate P,Q net injection power data
+    P_net, Q_net = get_net_injection_power(
+        BUS_DATA["P_g"], BUS_DATA["Q_g"], BUS_DATA["P_L"], BUS_DATA["Q_L"]
+    )
     
+    # BUS_DATA.info()
+    # LINE_DATA.info()
     
+    calc_admittance_matrix(BUS_DATA, LINE_DATA)
+    
+
     # while True : 
     # # Received MATRIX A data from the user
     #     MATRIX_A = input_matrix()   
