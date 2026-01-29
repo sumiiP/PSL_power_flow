cb90aff6500b9355acd9686ad1ed3c2959ef625c
sumin <parksm5117@gmail.com>
2026-01-29 22:52:17 +0900

    [FEAT] Maked a distinguish_input_unknown_data function

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -6,14 +6,16 @@ class DataLoader :
         self.lineFilePath = lineFilePath
         self.busHeader = busHeader
         self.lineHeader = lineHeader
-    
+    # %%
     def read_data_file(self, file_path, headers) :
         try : 
-            df = pd.read_csv(file_path, sep='\t', header=None, dtype='a')
+            df = pd.read_csv(file_path, sep='\t', header=None)
+            df = df.apply(pd.to_numeric, downcast='float', errors = 'coerce')
             
             if len(headers) == len(df.columns) : # add header list to dataframe
                 df.columns = headers
                 return df
+            
             else : 
                 print("please check the data files and Header information.")
                 return None
@@ -21,10 +23,21 @@ class DataLoader :
         except Exception as e :
             print(f"An error occurred while reading the file : {e}")
             return None
-            
-    # def make_dataframe(self, bud_header) : 
     
-    # def distinguish_input_unknown_data() : 
+    def distinguish_input_unknown_data(self, busDataFrame) : 
         
-    # def make_admittance_matrix() :
+        busDataFrame['P_net'] = busDataFrame['P_g'] - busDataFrame['P_L'] #calculate Net active Power
+        busDataFrame['Q_net'] = busDataFrame['Q_g'] - busDataFrame['Q_L'] #calculate Net reactive Power
+        
+        non_slack_id_list = busDataFrame[busDataFrame['Bus-type'] != 1]['ID'].tolist() #P mismatch judgment index
+        pd_bus_id_list = busDataFrame[busDataFrame['Bus-type'] == 3]['ID'].tolist() #Q mismatch judgment index
+        
+        id_info = {
+            'p_id' : non_slack_id_list,
+            'q_id' : pd_bus_id_list
+        }
+        
+        return busDataFrame, id_info
+        
+    def make_admittance_matrix() :
         
--- pf_manager.py
+++ pf_manager.py
@@ -6,10 +6,13 @@ class FlowManager :
     def run_power_flow(self) : 
         loader = DataLoader(BUS_FILE_PATH, LINE_FILE_PATH, BUS_FILE_HEADER_LIST, LINE_FILE_HEADER_LIST)
         
+        # make a bus and line dataframe
         busDataFrame = loader.read_data_file(BUS_FILE_PATH, BUS_FILE_HEADER_LIST)
         lineDataFrame = loader.read_data_file(LINE_FILE_PATH, LINE_FILE_HEADER_LIST)
-        
-        print(busDataFrame)
+        # print(busDataFrame)
+        # print(lineDataFrame)
+        loader.distinguish_input_unknown_data(busDataFrame)
+
 
 if __name__ == "__main__":
     manager = FlowManager()
